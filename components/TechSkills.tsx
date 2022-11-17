f75be9d6df60bcf84c0bb0f554881208674e4d10
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-11-17 21:43:19 +0530

    Update TechSkills.tsx

--- components/TechSkills.tsx
+++ components/TechSkills.tsx
@@ -44,21 +44,21 @@ const TechSkills = (props: Props) => {
       transition={{
         duration: 1.5,ease:"easeIn"
       }} className="grid  justify-center grid-cols-3 gap-10 lg:grid-cols-5 lg:gap-10 md:grid-cols-4 md:gap-8 xs:grid-cols-4 sm:gap-6 absolute top-60">
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/5968/5968350.png" lvl="90" stack="PYTHON"/>
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/6132/6132222.png" lvl="80" stack="C++"/>
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/3161/3161133.png" lvl="70" stack="SQL"/>
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/1260/1260667.png" lvl="60" stack="REACT"/>
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/226/226777.png" lvl="50" stack="JAVA"/>
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/174/174854.png" lvl="90" stack="HTML"/>
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/732/732190.png" lvl="80" stack="CSS"/>
-        <Skills tech="https://cdn-icons-png.flaticon.com/512/5968/5968672.png" lvl="80" stack="BOOTSTRAP"/>
-        <Skills tech="https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png" lvl="80" stack="TAILWIND"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/5968/5968350.png" lvl="90%" stack="PYTHON"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/6132/6132222.png" lvl="80%" stack="C++"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/3161/3161133.png" lvl="70%" stack="SQL"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/1260/1260667.png" lvl="60%" stack="REACT"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/226/226777.png" lvl="50%" stack="JAVA"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/174/174854.png" lvl="90%" stack="HTML"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/732/732190.png" lvl="80%" stack="CSS"/>
+        <Skills tech="https://cdn-icons-png.flaticon.com/512/5968/5968672.png" lvl="80%" stack="BOOTSTRAP"/>
+        <Skills tech="https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png" lvl="80%" stack="TAILWIND"/>
         <Skills tech="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" lvl="70" stack="JAVASCRIPT"/>
-        <Skills tech="https://seeklogo.com/images/N/numpy-logo-479C24EC79-seeklogo.com.png" lvl="80" stack="NUMPY"/>
-        <Skills tech="https://seeklogo.com/images/P/pandas-logo-776F6D45BB-seeklogo.com.png?v=637737823900000000" lvl="70" stack="PANDAS"/>
-        <Skills tech="https://seeklogo.com/images/M/matplotlib-logo-7676870AC0-seeklogo.com.png" lvl="70" stack="MATPLOTLIB"/>
-        <Skills tech="https://seeklogo.com/images/S/seaborn-logo-244EB2DEC5-seeklogo.com.png" lvl="70" stack="SEABORN"/>
-        <Skills tech="https://3.bp.blogspot.com/-yvrV6MUueGg/ToICp0YIDPI/AAAAAAAAADg/SYKg4dWpyC43AAfrDwBTR0VYmYT0QshEgCPcBGAYYCw/s1600/OpenCV_Logo.png" lvl="60" stack="OPENCV"/>
+        <Skills tech="https://seeklogo.com/images/N/numpy-logo-479C24EC79-seeklogo.com.png" lvl="805" stack="NUMPY"/>
+        <Skills tech="https://seeklogo.com/images/P/pandas-logo-776F6D45BB-seeklogo.com.png?v=637737823900000000" lvl="70%" stack="PANDAS"/>
+        <Skills tech="https://seeklogo.com/images/M/matplotlib-logo-7676870AC0-seeklogo.com.png" lvl="70%" stack="MATPLOTLIB"/>
+        <Skills tech="https://seeklogo.com/images/S/seaborn-logo-244EB2DEC5-seeklogo.com.png" lvl="70%" stack="SEABORN"/>
+        <Skills tech="https://3.bp.blogspot.com/-yvrV6MUueGg/ToICp0YIDPI/AAAAAAAAADg/SYKg4dWpyC43AAfrDwBTR0VYmYT0QshEgCPcBGAYYCw/s1600/OpenCV_Logo.png" lvl="60%" stack="OPENCV"/>
 
        
       </motion.div>
