732f64b61d2786101dd2e1d4a12d1ce301b6bdba
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2023-09-26 09:51:44 +0530

    Update expereince.js

--- utils/expereince.js
+++ utils/expereince.js
@@ -1,5 +1,14 @@
-export const experience = [{
+export const experience = [
+    {
         id: 1,
+        role: "Java Full stack Intern",
+        company: "Kodnest",
+        cmlogo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.crunchbase.com%2Forganization%2Fkodnest&psig=AOvVaw1aECST8ZQ_o7mUmg51dvYN&ust=1695788357780000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCKC9hoe2x4EDFQAAAAAdAAAAABAE",
+        duration: "July 23 - Present",
+        techstack: [ "Java","MySQL","HTML/CSS","React ...."],
+        tasks: ["",""],
+    },{
+        id: 2,
         role: "Datascience Intern",
         company: "Technologics Global Pvt Ltd",
         cmlogo: "https://media.licdn.com/dms/image/C560BAQFL7EEvDigbfA/company-logo_200_200/0/1633421686228?e=1685577600&v=beta&t=jNYsrxIqhsccrcEAdGGCqm8CHl0VfoQG8_d6zJotR7U",
@@ -9,7 +18,7 @@ export const experience = [{
 
     },
     {
-        id: 2,
+        id: 3,
         role: "Web Development Course",
         company: "Udemy Course",
         cmlogo: "https://media.licdn.com/dms/image/C4D0BAQFQr9e68bBOPQ/company-logo_200_200/0/1626275253364?e=1685577600&v=beta&t=9IxWePLh4M9tFsw7Ux63eI46gFPLF03IAC5t3by2iqA",
@@ -18,16 +27,6 @@ export const experience = [{
         tasks: ["Portfolio website", "Food restaurant website", "Youtube clone website"],
 
     },
-    // {
-    //     id: 3,
-    //     role: "Datascience Intern",
-    //     company: "Technologics Global Pvt Ltd",
-    //     cmlogo: "",
-    //     duration: "Aug 22 - Sep 22",
-    //     techstack: [ "python", "numpy", "pandas", "opencv .."],
-    //     tasks: ["done", "undone"],
-
-    // },
     // {
     //     id: 4,
     //     role: "Datascience Intern",
